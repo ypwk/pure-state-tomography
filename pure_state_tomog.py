c68a66a23bbca9bb8758e5250c339ff8b244d679
Kevin Wu <28658818+ypwk@users.noreply.github.com>
2023-09-11 13:43:47 -0400

    Implement MST smart CNOT

--- pure_state_tomog.py
+++ pure_state_tomog.py
@@ -7,6 +7,8 @@ from numpy import ndarray, array, sqrt, asarray, zeros
 from qiskit import QuantumCircuit, transpile, result, assemble
 from qiskit import Aer, execute
 
+import networkx as nx
+
 from enum import Enum
 
 
@@ -49,19 +51,35 @@ def fast_log2(num: int) -> int:
     return num.bit_length() - 1
 
 
-def infer_target(
-    target_idx, source_idx, source_val, n_qubits, h_measure, v_measure, op_pos
-) -> ndarray:
+def hamming(i1: int, i2: int) -> int:
+    """Computes the Hamming distance between two integers
+
+    Args:
+        i1 (int): integer 1
+        i2 (int): integer 2
+
+    Returns:
+        int: The Hamming distance between the two integers
+    """
+    x = i1 ^ i2
+    ones = 0
+
+    while x > 0:
+        ones += x & 1
+        x >>= 1
+
+    return ones
+
+
+def infer_target(target_idx, source_idx, source_val, h_measure, v_measure) -> ndarray:
     """Calculates and returns the value of an entry using previously inferred values in the measurement results.
 
     Args:
         target_idx (int): The index of the target value to infer
         source_idx (int): The index of the value to use to infer the target
         source_val (numpy.ndarray): The source value
-        n_qubits (int): The number of qubits needed to represent the vector
         h_measure (numpy.ndarray): The array of measurements with the Hadamard gate
         v_measure (numpy.ndarray): The array of measurements with the alternate gate
-        op_pos (int): Position of Hadamard/alternate operator in the tensor structure, zero indexed from the left
 
     Returns: numpy.ndarray
     """
@@ -155,6 +173,64 @@ class meas_manager:
         }
         pass
 
+    def add_cm(self, measure_type, cnots, op_pos=0) -> ndarray:
+        """Carries out a measurement on the pure state accounting for CNOT operations, returning the correct value.
+
+        Args:
+            measure_type (m_type): The type of operator used in the tensor structure
+            cnots (list): The list of locations to apply the CNOT operation
+            op_pos (int, optional): The operation of the position of the operator
+                    Defaults to 0, since it is unused when the operator is the identity
+        """
+
+        if measure_type not in [
+            m_type.cmplx_hadamard,
+            m_type.real_hadamard,
+            m_type.identity,
+        ]:
+            raise ValueError("Measurement type {} not valid.", measure_type)
+
+        state_circuit = self.m_state.copy("execute")
+        for a in range(self.m_state.num_qubits):
+            if self.m_state.num_qubits - a - 1 in cnots:
+                state_circuit.cnot(self.m_state.num_qubits - a - 1, self.m_state.num_qubits - a - 2)
+        for a in range(self.m_state.num_qubits):
+            if a == self.m_state.num_qubits - op_pos - 1:
+                if measure_type == m_type.real_hadamard:
+                    state_circuit.h(a)
+                elif measure_type == m_type.cmplx_hadamard:
+                    state_circuit.unitary(
+                        [
+                            [1 / sqrt(2), 1j / sqrt(2)],
+                            [1 / sqrt(2), -1j / sqrt(2)],
+                        ],
+                        a,
+                    )
+                elif measure_type == m_type.identity:
+                    state_circuit.i(a)
+            else:
+                state_circuit.i(a)
+
+        # SHOTS = 2048
+        # raw_result = run_circuit(circ_to_ex, SHOTS)
+
+        # using statevector for precise execution
+        simulator = Aer.get_backend("statevector_simulator")
+        raw_result = execute(state_circuit, simulator).result()
+        statevector = asarray(raw_result.get_statevector(state_circuit))
+
+        res = zeros(fast_pow(2, self.m_state.num_qubits))
+        for idx in range(len(statevector)):
+            res[idx] = abs(
+                statevector[idx].real * statevector[idx].real
+                - statevector[idx].imag * statevector[idx].imag
+            )
+
+        self.num_measurements += 1
+        self.__measurements[measure_type][op_pos] = res
+
+        return res
+
     def add_m(self, measure_type, op_pos=0) -> ndarray:
         """Carries out a measurement on the pure state, returning the correct value.
 
@@ -206,8 +282,32 @@ class meas_manager:
 
         self.num_measurements += 1
         self.__measurements[measure_type][op_pos] = res
-        # print("Measurement for {} and {}: {}".format(measure_type, op_pos, res), flush=True)
+
         return res
+
+    def fetch_cm(self, measure_type, cnots, op_pos) -> ndarray:
+        """Fetches a measurement result, denoted by location of and whether the Hadamard/alternate operator has been inverted in the tensor structure.
+        Also considers CNOT placement/
+
+        Args:
+            measure_type (m_type): The type of operator used in the tensor structure
+            cnots (list): The position of CNOTs in the tensor structure
+            op_pos (int): The position of the Hadamard in the tensor product, zero indexed from the left.
+
+        Returns:
+            measurement_result (numpy.ndarray):
+        """
+        if measure_type == m_type.identity:
+            return (
+                self.add_cm(measure_type=measure_type, cnots=cnots, op_pos=op_pos)
+                if self.__measurements[measure_type][0] is None
+                else self.__measurements[measure_type][0]
+            )
+        return (
+            self.add_cm(measure_type=measure_type, cnots=cnots, op_pos=op_pos)
+            if self.__measurements[measure_type][op_pos] is None
+            else self.__measurements[measure_type][op_pos]
+        )
 
     def fetch_m(self, measure_type, op_pos) -> ndarray:
         """Fetches a measurement result, denoted by location of and whether the Hadamard/alternate operator has been inverted in the tensor structure.
@@ -240,13 +340,12 @@ def pure_state_tomography(input_state=None, n_qubits=2):
         n_qubits (int, optional): The number of qubits needed to represent the input_vector. Defaults to 2.
     """
 
-    # TODO: Replace with meaningful input vector
     DIM = fast_pow(2, n_qubits)
     if input_state is None:  # generate random input vector if it is None
         # input_state = array([1 / 2, 1 / sqrt(2), 1 / sqrt(6), 1 / sqrt(12)])
         # input_state = array([1/2, -1/sqrt(2), 1/sqrt(6), 1/sqrt(12)])
-        input_state = array([1 / 2, 0, -2 / sqrt(6), 1 / sqrt(12)])
-        # input_state = array([1/2, 0, 0, -3/sqrt(12)])
+        # input_state = array([1 / 2, 0, -2 / sqrt(6), 1 / sqrt(12)])
+        input_state = array([1 / 2, 0, 0, -3 / sqrt(12)])
 
     print("Input vector: {}".format(input_state), flush=True)
 
@@ -254,7 +353,7 @@ def pure_state_tomography(input_state=None, n_qubits=2):
 
     # do initial identity measurement
     mm = meas_manager(m_state=mystery_state, n_qubits=n_qubits)
-    
+
     res = zeros((DIM, 2))
     __iter_inf_helper(res, mm)
 
@@ -273,7 +372,7 @@ def __iter_inf_helper(
         target_arr (numpy.ndarray): The array with incomplete measurements
         mm (meas_manager): Measure manager keeping track of measurement values.
     """
-    
+
     # do identity measurement to seed
     id_m = mm.add_m(m_type.identity, 0)
     counts = find_nonzero_positions(id_m)
@@ -281,14 +380,17 @@ def __iter_inf_helper(
     target_arr[counts[0]][0] = sqrt(id_m[0])
     t_list = set(counts)
     t_list.remove(counts[0])
-    
+
     counts = set(counts)
-    
+
     for op in range(mm.n_qubits):
         to_rem = set()
         for target in t_list:
             if target & (1 << (mm.n_qubits - op - 1)):
-                for source in [target - fast_pow(2, mm.n_qubits - op - 1), target + fast_pow(2, mm.n_qubits - op - 1)]:
+                for source in [
+                    target - fast_pow(2, mm.n_qubits - op - 1),
+                    target + fast_pow(2, mm.n_qubits - op - 1),
+                ]:
                     if source in counts and source not in t_list:
                         cmplx_m = mm.fetch_m(
                             measure_type=m_type.cmplx_hadamard, op_pos=op
@@ -301,38 +403,70 @@ def __iter_inf_helper(
                             target_idx=target,
                             source_idx=source,
                             source_val=target_arr[source],
-                            n_qubits=fast_log2(target_arr.shape[0]),
                             h_measure=real_m,
                             v_measure=cmplx_m,
-                            op_pos=op,
                         )
                         to_rem.add(target)
             else:
-                if target + fast_pow(2, mm.n_qubits - op - 1) in counts and target + fast_pow(2, mm.n_qubits - op - 1) not in t_list:
-                    
-                    cmplx_m = mm.fetch_m(
-                        measure_type=m_type.cmplx_hadamard, op_pos=op
-                    )
-                    real_m = mm.fetch_m(
-                        measure_type=m_type.real_hadamard, op_pos=op
-                    )
+                if (
+                    target + fast_pow(2, mm.n_qubits - op - 1) in counts
+                    and target + fast_pow(2, mm.n_qubits - op - 1) not in t_list
+                ):
+                    cmplx_m = mm.fetch_m(measure_type=m_type.cmplx_hadamard, op_pos=op)
+                    real_m = mm.fetch_m(measure_type=m_type.real_hadamard, op_pos=op)
 
                     target_arr[target] = infer_target(
                         target_idx=target,
                         source_idx=target + fast_pow(2, mm.n_qubits - op - 1),
-                        source_val=target_arr[target + fast_pow(2, mm.n_qubits - op - 1)],
-                        n_qubits=fast_log2(target_arr.shape[0]),
+                        source_val=target_arr[
+                            target + fast_pow(2, mm.n_qubits - op - 1)
+                        ],
                         h_measure=real_m,
                         v_measure=cmplx_m,
-                        op_pos=op,
                     )
                     to_rem.add(target)
-            
+
             t_list = t_list - to_rem
-            
+
             if len(t_list) == 0:
                 return
-    
+
+    # use MST to deal with infeasible ones
+    graph = nx.complete_graph(counts)
+    for a in graph.nodes():
+        for b in graph.nodes():
+            if a != b:
+                graph[a][b]["weight"] = hamming(a, b)
+    mst = nx.minimum_spanning_tree(graph)
+
+    for n in t_list:
+        # find best source
+        edges = sorted(mst.edges(data=True), key=lambda node: node[2].get("weight", 1))
+        cm_idx = 0
+        mwe = edges[cm_idx]
+        source = mwe[0] if mwe[1] == n else mwe[1]
+        while source in t_list:
+            mwe = edges[cm_idx]
+            source = mwe[0] if mwe[1] == n else mwe[1]
+
+        # construct measure operators with correct CNOT placement
+        output = [int(x) for x in "{:0{size}b}".format(source ^ n, size=mm.n_qubits)]
+        cnots = find_nonzero_positions(output)
+        op_pos = cnots[0]
+        cnots = cnots[1:]
+
+        real_m = mm.fetch_cm(m_type.real_hadamard, cnots, op_pos)
+        cmplx_m = mm.fetch_cm(m_type.cmplx_hadamard, cnots, op_pos)
+        
+        # infer target
+        target_arr[n] = infer_target(
+            target_idx=n ^ (1 << op_pos), # bit shift to ensure correct structure
+            source_idx=source,
+            source_val=target_arr[source],
+            h_measure=real_m,
+            v_measure=cmplx_m,
+        )
+
 
 if __name__ == "__main__":
     print(" Done!")
